ea32d225e52f47de3ae59e7cb027eb7701edf325
Kid R <rainui.ly@gmail.com>
2022-02-16 03:01:17 +0100

    new: create dbconnector with mariadb
    [PFE-17]
    - new: adapt existent method with mariadb connector
    - new: possible to create a user account and login

--- db/volume/initdb.d/2-init_user.dev.sql
+++ db/volume/initdb.d/2-init_user.dev.sql
@@ -1,2 +1,4 @@
 CREATE USER 'sensei' @localhost IDENTIFIED BY 'sensei';
-GRANT ALL PRIVILEGES ON dojoplateforme.* TO 'sensei'@localhost;
+CREATE USER 'sensei' @0.0.0.0 IDENTIFIED BY 'sensei';
+GRANT ALL PRIVILEGES ON dojoplateforme.* TO 'sensei' @localhost;
+GRANT ALL PRIVILEGES ON dojoplateforme.* TO 'sensei' @0.0.0.0;
---
+++ db/volume/initdb.d/3-default_data.dev.sql
@@ -0,0 +1,6 @@
+USE dojo_db;
+
+INSERT INTO User (username,email,hashPassword)
+VALUES 
+    ('admin','admin@dojo.dev', "$2a$12$HIpKNQIPL16scuo5.OO1bOIV5uogmMsrEvWYqH/bgLNmCAr6cTbNa"),
+    ('sensei','sensei@dojo.dev', "$2a$12$8/23TbZKxiBzmdtb2MpB.uXP83MckK8uuIoeZBxlAn7jSqdTZgNzm");
