d7c2ab40805e020b5d70e1984c962b12d6b3b70a
Kid R <rainui.ly@gmail.com>
2022-02-16 21:22:36 +0100

    new: add nodejs api test/
    [PFE-23]
    - new: create API auth tests using mochajs
    - ref: general improvements with mariadb
    - ref: removes useless packages

--- api/api_auth/plugins/dbconnector.js
+++ api/api_auth/plugins/dbconnector.js
@@ -2,11 +2,11 @@ const mariadb = require("mariadb");
 require("dotenv").config({ path: "./.dev.env" });
 
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT,
+    host: process.env.API_DB_HOST,
+    user: process.env.API_DB_USER,
+    password: process.env.API_DB_PASSWORD,
+    database: process.env.API_DB_DATABASE,
+    connectionLimit: process.env.API_DB_CONNECTION_LIMIT,
 });
 
 const __hasUserBy = async (byCallback, args) => {
